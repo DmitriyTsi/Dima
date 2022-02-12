215aa3d9c22d12ce141922d7a59a8b92630d4af6
Dima <arak85ss@gmail.com>
2022-02-13 01:03:17 +0200

    Hello

---
+++ src/Test/T1.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class T1 {
+    static void Good() {
+        System.out.println("Good Bye!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello WWW");
+        Good();
+    }
+}
